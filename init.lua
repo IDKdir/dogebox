776a39f30b859267c4abece3fecea24aec21be48
IDKdir <jysinok@gmail.com>
2025-01-03 12:30:33 -0500

    Fixed errors

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-getgenv().__ESP__ = false
+local branch = 'main'
 
 getgenv().Import = function(path)
     return loadstring(game:HttpGet(('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/%s/%s.lua'):format(branch, path)))()
