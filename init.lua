d3702baa432fdd4c1a80a0c392f4fd04d8b58ef0
IDKdir <jysinok@gmail.com>
2025-01-03 12:31:34 -0500

    Fixed global function

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
-local branch = 'main'
-
 getgenv().Import = function(path)
+    local branch = 'main'
     return loadstring(game:HttpGet(('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/%s/%s.lua'):format(branch, path)))()
 end
 
