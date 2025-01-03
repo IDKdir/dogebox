d112c12e1046e1ce33c92ab2a5fd71a77ac46c99
IDKdir <jysinok@gmail.com>
2025-01-03 12:43:16 -0500

    Forgot ui

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/main/ui/main.lua'))()
 loadstring(game:HttpGet('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/main/modules/esp.lua'))()
