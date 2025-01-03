12d5faef62d721ed3062d6e4cf45c572be315f5e
IDKdir <jysinok@gmail.com>
2025-01-03 12:35:44 -0500

    Debug

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@ function Import(path)
     return loadstring(game:HttpGet(('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/%s/%s.lua'):format(branch, path)))()
 end
 
+print('Hello from init')
 Import('modules/esp')
