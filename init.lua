3a33a90ed98b0ef15921033b0f88908be52a12f5
IDKdir <jysinok@gmail.com>
2025-01-03 12:45:16 -0500

    Work please

--- init.lua
+++ init.lua
@@ -1,2 +1,34 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/main/ui/main.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/IDKdir/dogebox/refs/heads/main/modules/esp.lua'))()
+Iris = loadstring(game:HttpGet('https://raw.githubusercontent.com/x0581/Iris-Exploit-Bundle/main/bundle.lua'))().Init(game:GetService('CoreGui'))
+
+task.spawn(function()
+    local Players = game:GetService('Players')
+    local RunService = game:GetService('RunService')
+
+    local LocalPlayer = Players.LocalPlayer
+
+    RunService.Heartbeat:Connect(function()
+        for i, v in pairs(Players:GetPlayers()) do
+            if v == LocalPlayer then continue end
+            if not v.Character then continue end
+            local Character = v.Character
+            local Highlight = Character:FindFirstChild('DogeboxHighlight')
+
+            if not Highlight then
+                Highlight = Instance.new('Highlight', Character)
+                Highlight.Name = 'DogeboxHighlight'
+            end
+            Highlight.FillColor = v.TeamColor.Color
+            Highlight.Enabled = getgenv().__ESP__
+        end
+    end)
+end)
+
+Iris:Connect(function()
+    local EspChecked = Iris.State(false)
+
+    Iris.Window({'Naval Warfare'})
+        Iris.Checkbox({'ESP'}, {isChecked = EspChecked})
+    Iris.End()
+
+    getgenv().__ESP__ = EspChecked.value
+end)
